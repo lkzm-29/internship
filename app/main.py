25eca842e2f676623b1a8581ff423094918837ee
Ubuntu <azureuser@melior.4twfzs3haesedows3porspcpog.tx.internal.cloudapp.net>
2026-07-13 13:19:53 +0000

    Add frontend, enable CORS

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def hello():
     return {"message": "Hello, World! - Melior DevSecOps Internship"}
-
